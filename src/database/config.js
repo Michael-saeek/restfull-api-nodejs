fdacbec8e8087a1aa3fde25875d004ccc953c229
Michael Andrade Davila <76621748+Michael-saeek@users.noreply.github.com>
2022-10-15 20:59:02 -0300

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose')
 const URIaddress = process.env.MONGO_DB_ATLAS
 
-/*
-mongodb+srv://michaeladmin:24931722mmm@clusteritemsarts.ubdc5.mongodb.net/test
-*/
-
-
 const dbConnnection = async () => {
 
     console.log(typeof URIaddress, + " ", URIaddress)
